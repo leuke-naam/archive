9323d7380a3823afb7c2b7e11e6c215eb5a7fd1b
Semele <hello@semele.fyi>
2025-11-13 16:27:54 +0100

    feat: renaem titels

--- deploy.sh
+++ deploy.sh
@@ -16,9 +16,9 @@ for dir in execut-*; do
   cd temp/$dir
   git init
   git add .
-  git switch -c archive
+  git branch -M archive
   git commit -m "feat: initial commit 🥳"
   git remote add origin git@github.com:stichting-sticky/$dir.git
-  git push -u origin archive
+  # git push -u origin archive
   cd ../../
 done
