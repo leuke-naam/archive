b838b273b748e35d3a447644f3b83d9f68f7e1d9
Semele <hello@semele.fyi>
2025-12-16 11:27:57 +0100

    chore: move branch

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,28 @@
 rm -rf temp/
 
-# Clean project and create bun.lock
-for dir in execut-*; do
-  cd $dir
-  # rm -rf .git/ .astro/ dist/ node_modules/ bun.lock
-  # bun install
-  # bun update
-  # rm -rf node_modules/
-  # bun install
-  bun run build
-  # bun run preview
-  cd ../
-done
-
-# mkdir temp
-
+# # Clean project and create bun.lock
 # for dir in execut-*; do
-#   cp -r $dir temp/
-#   cd temp/$dir
-#   git init
-#   git add .
-#   git branch -M archive
-#   git commit -m "feat: initial commit 🥳"
-#   git remote add origin git@github.com:stichting-sticky/$dir.git
-#   git push -u origin archive
-#   cd ../../
+#   cd $dir
+#   rm -rf .git/ .astro/ dist/ node_modules/ bun.lock
+#   bun install
+#   bun update
+#   rm -rf node_modules/
+#   bun install
+#   bun run build
+#   bun run preview
+#   cd ../
 # done
+
+mkdir temp
+
+for dir in execut-*; do
+  cp -r $dir temp/
+  cd temp/$dir
+  git init
+  git add .
+  git branch -M archive
+  git commit -m "feat: initial commit 🥳"
+  git remote add origin git@github.com:stichting-sticky/$dir.git
+  git push -u origin archive
+  cd ../../
+done
